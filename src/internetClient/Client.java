dadb2925ed8420e1037637d381d1cd6b42f9bb79
hilde <hildehamer@hotmail.com>
2015-09-08 20:35:04 +0200

    De thread is een innerclas geworden.

--- src/internetClient/Client.java
+++ src/internetClient/Client.java
@@ -1,7 +1,9 @@
 package internetClient;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -10,39 +12,78 @@ import java.util.Scanner;
 
 public class Client {
 	private static Socket socket;
+	private static final String HOST="localhost" ;
+	private static final int GATE_NUMBER=9004;
+
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		boolean validUserName = false;
 		Scanner usernamescanner = new Scanner(System.in);
 		String userName = "";
-		while(!validUserName){
+		
+		Client client= new Client();
+		
+		//Invoeren gebruikersnaam
+		while (!validUserName) {
 			System.out.println("voer een gebruikersnaam in");
 			userName = usernamescanner.nextLine();
-			if(!userName.isEmpty()){
+			if (!userName.isEmpty()) {
 				validUserName = true;
 			} else {
 				System.out.println("ongeldige gebruikersnaam");
 			}
-			
+
 		}
-		socket = new Socket("localhost", 9004);
 		
-		ClientListeningThread thread = new ClientListeningThread(socket);
+		socket = new Socket(HOST, GATE_NUMBER);
+
+		
+		ClientListeningThread thread = client.new ClientListeningThread(socket);
 		thread.start();
-		
+
+		//zending the username
 		OutputStream outputStream = socket.getOutputStream();
 		PrintWriter writer = new PrintWriter(outputStream);
 		writer.println(userName);
 		writer.flush();
 		System.out.println("om naar een persoon te wisperen type w:/<gebruikersnaam v/d persoon> <text>");
-		while(true){
+		
+		//berichten inlezen en versturen
+		while (true) {
 			OutputStream outputStrm = socket.getOutputStream();
 			String text = usernamescanner.nextLine();
 			writer = new PrintWriter(outputStrm);
-			writer.println( text );
+			writer.println(text);
 			writer.flush();
-			
+
 		}
 
+	}
+	
+	/**
+	 * Thread die de ontvangen berichte afhandelt
+	 */
+	public class ClientListeningThread extends Thread {
+		
+		private Socket socket;
+		public ClientListeningThread(Socket socket){
+			this.socket = socket;
+		}
+		
+		public void run() {
+			while (true) {
+				try {
+					InputStream inputStream = socket.getInputStream();
+					BufferedReader reader = new BufferedReader(
+							new InputStreamReader(inputStream));
+					System.out.println(reader.readLine());
+
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+			}
+		}
 
 	}
 
